2512054038e28501be53cdeaea1c72c5b2c49ac3
vberder <vincent.berder@al-enterprise.com>
2020-04-10 14:33:53 +0200

    Update

--- tests/index.ts
+++ tests/index.ts
@@ -1,9 +1,11 @@
 import "mocha";
 import  * as assert from "assert";
+import {RainbowBodeSDKTest} from "../dist";
 
 describe("index", ()=> {
     it('should say MAIN - start', function () {
-        console.log("test MAIN - start");
+        //console.log("test MAIN - start");
+        RainbowBodeSDKTest.Main();
         assert.ok(true);
     });
 })

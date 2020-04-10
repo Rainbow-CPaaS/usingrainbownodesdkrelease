daf9b5d5d3e6dadea9c1b476d3c07963f5a482c4
vberder <vincent.berder@al-enterprise.com>
2020-04-10 14:09:42 +0200

    initial project

---
+++ tests/index.ts
@@ -0,0 +1,9 @@
+import "mocha";
+import  * as assert from "assert";
+
+describe("index", ()=> {
+    it('should say MAIN - start', function () {
+        console.log("test MAIN - start");
+        assert.ok(true);
+    });
+})
